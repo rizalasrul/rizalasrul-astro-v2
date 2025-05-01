c8d289ece4305f23643419d6c0176d749c6fc521
Rizal Asrul Pambudi <rizal.asrul@bukalapak.com>
2025-05-01 21:07:06 +0700

    feat: convert .md to .mdx

--- .netlify/v1/functions/ssr/.netlify/build/chunks/consts_pymCaDPq.mjs
+++
@@ -1,6 +0,0 @@
-const SITE_TITLE = "Rizal Asrul Pambudi";
-const SITE_DESCRIPTION = "Welcome to my website!";
-const HOMEBLOG_TITLE = "Documenting the journey";
-const HOMEBLOG_DESCRIPTION = "Tech insights & life reflections";
-
-export { HOMEBLOG_TITLE as H, SITE_DESCRIPTION as S, SITE_TITLE as a, HOMEBLOG_DESCRIPTION as b };
