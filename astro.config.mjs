834255a93ae2a6df34adf6296cfa98c7fb16cc1d
Rizal Asrul Pambudi <rizal.asrul@live.com>
2025-05-07 19:19:41 +0700

    feat: publish new post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-
-import icon from "astro-icon";
-
-import netlify from "@astrojs/netlify";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
